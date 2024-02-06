dad0c37f225c3537c3f0594eb8cd047cc303c6ec
thangtran-c0x12c <thang.tran@c0x12c.com>
2024-02-06 12:12:41 +0700

    Add AxiosNumerator middleware

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,7 +2,7 @@ import NumeratorClient from './client';
 import { NumeratorProvider, useNumeratorContext } from './provider';
 export * from './util';
 export * from './client/type.client';
-export * from './client/axios.middleware';
 export * from './provider/type.provider';
+import AxiosNumerator from './client/axios.middleware';
 
-export { NumeratorClient, NumeratorProvider, useNumeratorContext };
+export { NumeratorClient, NumeratorProvider, useNumeratorContext, AxiosNumerator };
