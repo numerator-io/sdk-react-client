e76f087ddb2a5f49c825ee10917e7e2f5a042f55
Thang Tran <125898198+spartan-thangtran@users.noreply.github.com>
2024-03-28 12:21:05 +0700

    Update index.tsx

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -4,4 +4,5 @@ export * from './util';
 export * from './client/type.client';
 export * from './provider/type.provider';
 
+// Test GA
 export { NumeratorClient, NumeratorProvider, useNumeratorContext };
