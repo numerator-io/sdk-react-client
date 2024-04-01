6c83d4f8db10ab894bab6bdc9f4abcd8a5e6e394
thangtran-c0x12c <thang.tran@c0x12c.com>
2024-04-01 21:03:10 +0700

    Update GitHub Actions checkout action version

--- tools/create_release.sh
+++ tools/create_release.sh
@@ -2,10 +2,16 @@
 
 SEMTAG='./tools/semtag'
 ACTION=${1:-patch}
+# If true it will add the user prefix, e.g. 1.0.0 -> v1.0.0 or v1.0.0 -> v1.0.0
+V_PREFIX=${2:-false}
 
 git fetch origin --tags
 
 RELEASE_VERSION="$($SEMTAG final -s $ACTION -o)"
+
+if [ "$V_PREFIX" = false ]; then
+  RELEASE_VERSION=$(echo $RELEASE_VERSION | sed 's/v//g')
+fi
 
 echo "Next release version: $RELEASE_VERSION"
 
