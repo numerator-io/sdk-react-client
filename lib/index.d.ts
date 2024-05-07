fe8d80014771a8b79aa2045991d78493d5389df8
spartan-khanhvunguyen <khanh.vu.nguyen@c0x12c.com>
2024-05-07 10:41:26 +0700

    update bundle files

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -238,45 +238,134 @@ declare const useNumeratorContext: () => NumeratorContextType;
 interface MockFlag {
     key: string;
     value: any;
-    context: Record<string, any>;
+    context?: Record<string, any>;
 }
+/**
+ * Sets the mocked feature flags to the provided array of flags.
+ * If a flag's context is not provided, it defaults to an empty object.
+ *
+ * @param flags An array of MockFlag objects representing the feature flags to be mocked.
+ */
 declare const mockFlags: (flags: MockFlag[]) => void;
+/**
+ * Adds a mocked feature flag to the list of mocked flags.
+ * If the flag's context is not provided, it defaults to an empty object.
+ *
+ * @param flag The MockFlag object representing the feature flag to be added.
+ */
 declare const addMockedFlag: (flag: MockFlag) => void;
+/**
+ * Removes a mocked feature flag with the specified key and context from the list of mocked flags.
+ * If no context is provided, it removes the flag regardless of its context.
+ *
+ * @param key The key of the feature flag to be removed.
+ * @param context (Optional) The context of the feature flag to be removed.
+ */
 declare const removeMockedFlag: (key: string, context?: Record<string, any>) => void;
 interface MockNumeratorProviderProps {
-    /**
-     * The configuration client instance used by the NumeratorProvider.
-     */
     configClient?: ConfigClient;
-    /**
-     * The default context client send to NumeratorProvider
-     */
     defaultContext?: Record<string, any>;
-    /**
-     * Start to load polling
-     */
     loadPolling?: boolean;
 }
 declare const useMockNumeratorProvider: (props?: MockNumeratorProviderProps) => {
+    /**
+     * Retrieve the feature flag value, working the same as the function provided in useNumeratorContext.
+     *
+     * @param key The key of the feature flag.
+     * @param defaultVal The default value to return if the feature flag is not found.
+     * @param context (Optional) The context for the feature flag.
+     * @param useDefaultContext (Optional) Specifies whether to use the default context if no context is provided. Defaults to true.
+     */
     getFeatureFlag: jest.Mock<Promise<any>, [key: any, defaultVal: any, context?: any, useDefaultContext?: any], any>;
+    /**
+     * Retrieve the feature flag type BOOLEAN, working the same as the function provided in useNumeratorContext.
+     *
+     * @param key The key of the feature flag.
+     * @param defaultVal The default value to return if the feature flag is not found.
+     * @param context (Optional) The context for the feature flag.
+     * @param useDefaultContext (Optional) Specifies whether to use the default context if no context is provided. Defaults to true.
+     */
     booleanFlagVariationDetail: jest.Mock<Promise<any>, [key: any, defaultVal: any, context?: any, useDefaultContext?: any], any>;
+    /**
+     * Retrieve the feature flag type NUMBER (LONG or DOUBLE), working the same as the function provided in useNumeratorContext.
+     *
+     * @param key The key of the feature flag.
+     * @param defaultVal The default value to return if the feature flag is not found.
+     * @param context (Optional) The context for the feature flag.
+     * @param useDefaultContext (Optional) Specifies whether to use the default context if no context is provided. Defaults to true.
+     */
     numberFlagVariationDetail: jest.Mock<Promise<any>, [key: any, defaultVal: any, context?: any, useDefaultContext?: any], any>;
+    /**
+     * Retrieve the feature flag type STRING, working the same as the function provided in useNumeratorContext.
+     *
+     * @param key The key of the feature flag.
+     * @param defaultVal The default value to return if the feature flag is not found.
+     * @param context (Optional) The context for the feature flag.
+     * @param useDefaultContext (Optional) Specifies whether to use the default context if no context is provided. Defaults to true.
+     */
     stringFlagVariationDetail: jest.Mock<Promise<any>, [key: any, defaultVal: any, context?: any, useDefaultContext?: any], any>;
+    /**
+     * Retrieve the default context, working the same as the function provided in useNumeratorContext.
+     */
     getDefaultContext: jest.Mock<Record<string, any>, [], any>;
+    /**
+     * Clear default context, working the same as the function provided in useNumeratorContext.
+     */
     clearDefaultContext: jest.Mock<{}, [], any>;
+    /**
+     * Add another default context, working the same as the function provided in useNumeratorContext.
+     */
     addDefaultContextValue: jest.Mock<{}, [key: any, value: any], any>;
+    /**
+     * Remove an existing default context, working the same as the function provided in useNumeratorContext.
+     */
     removeDefaultContextValue: jest.Mock<void, [key: any], any>;
+    /**
+     * Update cache flags for faster retrieval of flag with default context, working the same as the function provided in useNumeratorContext.
+     */
     fetchPollingFeatureFlag: jest.Mock<Promise<void>, [], any>;
+    /**
+     * Currently not supported, but you might define on your own.
+     */
     initFeatureFlag: jest.Mock<any, any, any>;
+    /**
+     * Currently not supported, but you might define on your own.
+     */
     featureFlags: jest.Mock<any, any, any>;
+    /**
+     * Currently not supported, but you might define on your own.
+     */
     flagValueByKey: jest.Mock<any, any, any>;
+    /**
+     * Currently not supported, but you might define on your own.
+     */
     startPolling: jest.Mock<any, any, any>;
+    /**
+     * Currently not supported, but you might define on your own.
+     */
     stopPolling: jest.Mock<any, any, any>;
+    /**
+     * Currently not supported, but you might define on your own.
+     */
     restartPolling: jest.Mock<any, any, any>;
+    /**
+     * Currently not supported, but you might define on your own.
+     */
     handleFlagUpdated: jest.Mock<any, any, any>;
+    /**
+     * Currently not supported, but you might define on your own.
+     */
     handleFlagUpdatedError: jest.Mock<any, any, any>;
+    /**
+     * Current cache flags.
+     */
     cacheFlags: Record<string, any>;
 };
+/**
+ * Resets the mocked flags array and mocks for NumeratorProvider.
+ *
+ * It clears the mockedFlags array and resets all mocked functions provided by useMockNumeratorProvider.
+ */
 declare const resetNumeratorMocks: () => void;
 
 export { type ApiClientInterface, type ApiRequestOptions, type ApiResponse, type ConfigClient, type ErrorResponse, type FeatureFlagConfig, type FeatureFlagConfigListingRequest, type FeatureFlagConfigListingResponse, type FeatureFlagPollingResponse, type FeatureFlagValueByKeyRequest, type FlagCollection, type FlagEvaluationDetail, FlagStatusEnum, type FlagUpdatedCallback, type FlagUpdatedErrorCallback, FlagValueTypeEnum, type FlagVariationValue, type MockNumeratorProviderProps, NumeratorClient, type NumeratorContextType, NumeratorProvider, type NumeratorProviderProps, type PaginationRequest, type PaginationResponse, type VariationKeyType, type VariationValue, addMockedFlag, mockFlags, removeMockedFlag, resetNumeratorMocks, useMockNumeratorProvider, useNumeratorContext };
