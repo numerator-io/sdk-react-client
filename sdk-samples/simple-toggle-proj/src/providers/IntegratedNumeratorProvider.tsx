78abc74a852db1c7ca96439477dec8f3785d690a
spartan-khanhvunguyen <khanh.vu.nguyen@c0x12c.com>
2024-05-13 11:55:14 +0700

    custom provider

--- sdk-samples/simple-toggle-proj/src/providers/IntegratedNumeratorProvider.tsx
+++ sdk-samples/simple-toggle-proj/src/providers/IntegratedNumeratorProvider.tsx
@@ -1,13 +1,18 @@
 import React, { useEffect, createContext, useContext } from 'react';
 import { useNumeratorContext } from '@numerator-io/sdk-react-client';
-
 interface IntegratedNumeratorContextType {
   /**
-   * Get feature flag value.
+   * Get feature flag value when polling is enabled.
    * @param key - The flag key of the feature flag to fetch value for.
    * @param defaultVal - Default value of string value if not get flag variation
    */
-  checkEnabledFeatureFlag(key: string, defaultVal?: boolean): Promise<boolean>;
+  checkEnabledFeatureFlag(key: string, defaultVal?: boolean): boolean;
+  /**
+   * Get feature flag value async when polling is disabled.
+   * @param key - The flag key of the feature flag to fetch value for.
+   * @param defaultVal - Default value of string value if not get flag variation
+   */
+  checkAsyncEnabledFeatureFlag(key: string, defaultVal?: boolean): Promise<boolean>;
 }
 
 // Create a context for the SDK
@@ -20,13 +25,19 @@ type Props = {
 export const IntegratedNumeratorProvider: React.FC<Props> = ({ children }) => {
   const { handleFlagUpdated, cacheFlags, getFeatureFlag } = useNumeratorContext();
 
-  const checkEnabledFeatureFlag = async (key: string, defaultVal?: boolean) => {
-    const defaultValue = cacheFlags[key].value.booleanValue ?? defaultVal;
+  // Utilize when polling is disabled (OFF)
+  const checkAsyncEnabledFeatureFlag = async (key: string, defaultVal?: boolean) => {
+    const defaultValue = cacheFlags[key]?.value.booleanValue ?? defaultVal;
     const enabled = await getFeatureFlag(key, defaultValue);
     return enabled;
   };
 
-  // Subscribe to flag updates to adjust the displayed animal type based on the 'enable_land_pet' flag.
+  // Utilize when polling is enabled (ON)
+  const checkEnabledFeatureFlag = (key: string, defaultVal?: boolean): boolean => {
+    return cacheFlags[key]?.value.booleanValue ?? defaultVal ?? false;
+  };
+
+  // Subscribe to flag updates
   useEffect(() => {
     // Register a callback function to handle flag updates
     const unregister = handleFlagUpdated((flags) => {
@@ -41,6 +52,7 @@ export const IntegratedNumeratorProvider: React.FC<Props> = ({ children }) => {
     <IntegratedNumeratorContext.Provider
       value={{
         checkEnabledFeatureFlag,
+        checkAsyncEnabledFeatureFlag,
       }}>
       {children}
     </IntegratedNumeratorContext.Provider>
